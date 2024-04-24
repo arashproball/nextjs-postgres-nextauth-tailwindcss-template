eacebed832e2dfb5e9ea3e6e3e04855939d8a629
Amir <ghafourzadea@gmail.com>
2024-04-24 10:55:02 +0330

    Initial for aasstone

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,25 @@ import { getUsers } from '@/lib/db';
 import { UsersTable } from './users-table';
 import { Search } from './search';
 
+interface User {
+  id: number;
+  name: string | null;
+  username: string | null;
+  email: string | null;
+}
+
+const users: User[] = [
+  { id: 1, name: 'gholi', email: 'gholi@hashem.com', username: 'gholi' },
+  { id: 2, name: 'gholi', email: 'gholi@hashem.com', username: 'gholi' },
+  { id: 3, name: 'gholi', email: 'gholi@hashem.com', username: 'gholi' },
+  { id: 4, name: 'gholi', email: 'gholi@hashem.com', username: 'gholi' },
+  { id: 5, name: 'gholi', email: 'gholi@hashem.com', username: 'gholi' },
+  { id: 6, name: 'gholi', email: 'gholi@hashem.com', username: 'gholi' },
+  { id: 7, name: 'gholi', email: 'gholi@hashem.com', username: 'gholi' },
+  { id: 8, name: 'gholi', email: 'gholi@hashem.com', username: 'gholi' },
+  { id: 9, name: 'gholi', email: 'gholi@hashem.com', username: 'gholi' }
+];
+
 export default async function IndexPage({
   searchParams
 }: {
@@ -9,7 +28,6 @@ export default async function IndexPage({
 }) {
   const search = searchParams.q ?? '';
   const offset = searchParams.offset ?? 0;
-  const { users, newOffset } = await getUsers(search, Number(offset));
 
   return (
     <main className="flex flex-1 flex-col p-4 md:p-6">
@@ -19,7 +37,7 @@ export default async function IndexPage({
       <div className="w-full mb-4">
         <Search value={searchParams.q} />
       </div>
-      <UsersTable users={users} offset={newOffset} />
+      <UsersTable users={users} offset={10} />
     </main>
   );
 }
